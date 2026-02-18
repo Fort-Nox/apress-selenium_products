29e175771111c7ebd651b01a129bad5a3b7c9e35
Fort-Nox <sadovskov.andrew@gmail.com>
2026-02-18 17:57:36 +0500

    fix/test

--- lib/pages/company_site/eti/popups/groups_binding_popup.rb
+++ lib/pages/company_site/eti/popups/groups_binding_popup.rb
@@ -5,19 +5,15 @@ module CompanySite
     class Table
       class GroupsBindingPopup < self
         div(:popup, css: '.groups-popup__title')
-        elements(:groups_tree, css: '.groups-popup__tree li')
+        elements(:groups_tree, css: '.js-group-item')
         button(:save, css: '.groups-popup__save-button')
 
         def wait_for_visible
           popup_element.when_visible
         end
 
-        # TODO: Надо бы пофиксить, чтобы с другими спеками было идентично
-        # С методом find не проходит. Кажется не видит элементы из списка
         def select_group(name)
-          # groups_tree_elements.find { |group| group.text.strip == name }.click
-          Page.button(:product_group, xpath: "//*[@id='popup-content']//*[contains(text(), '#{name}')]")
-          product_group
+          groups_tree_elements.find { |group| group.text.gsub(/\s+|\(\d+\)/, '') == name.gsub(/\s+/, '') }.click
           save
         end
       end
